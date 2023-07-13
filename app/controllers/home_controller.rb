e385b06db8513f31ea545ff6010e1b34fd95df07
isaacbell1017 <isaacbell1017@gmail.com>
2023-07-12 21:38:48 -0400

    Cache our weather forecasts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
-    $redis.set('key', 'value')
-    @test = $redis.get('key')
-  end
-
-  def weather
   end
 end
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,22 @@
+require 'json'
+
 class WeatherController < ApplicationController
   # before_action :authenticate_user!
 
   def forecast
-    location = params.require(:location)
-    weather = Weather.weather_for(location)
+    location = params.require(:location).to_s
+
+    zip_code = location =~ /\D/ ? location.to_zip.first : location
+
+    if json = $redis.get(zip_code)
+      weather = JSON.parse(json).with_indifferent_access
+      weather[:from_cache] = true
+    else
+      weather = Weather.weather_for(zip_code)
+      $redis.set(zip_code, weather.to_json, ex: 30 * 60) # Cache for 30 minutes
+      weather[:from_cache] = false
+    end
+
     render json: weather
   end
 end
